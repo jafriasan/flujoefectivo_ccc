72eba2aa9d226e45cac5ec19916f4cc0c1f9c004
Judith Frias <jafriaso@gmail.com>
2026-08-30 17:38:53 -0600

    Update flujos_dashboard.py
    Se cambio ruta de archivo insumo

--- flujos_dashboard.py
+++ flujos_dashboard.py
@@ -22,7 +22,7 @@ def dashboard_series_flujo():
     # CARGA DE DATOS
     # =====================================================
 
-    archivo = "/Users/judith_frias/Flujo_pto_data/flujos_cubo.xlsx"
+    archivo = "flujos_cubo.xlsx"
 
     @st.cache_data
     def load_data():
